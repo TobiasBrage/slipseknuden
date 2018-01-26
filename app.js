0d2f5cae0d76b15df53e1dea71dce6de7f862ce1
Tobias Brage <gamerfreaq@live.com>
2018-01-26 11:27:13 +0100

    slipseknuden

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express')
+  , routes = require('./routes')
+  , data = require('./routes/data')
+  , http = require('http')
+  , path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var mysql      = require('mysql');
+var cookieParser = require('cookie-parser');
+var bodyParser=require("body-parser");
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'test',
+  port : 8889
+});
+
+connection.connect();
+
+global.db = connection;
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routes.index);
+app.get('/om', data.about);
+app.get('/garanti', data.insurance);
+app.get('/kontakt', data.contact);
+app.get('/nyheder', data.news);
+app.get('/produkter', data.products);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/data.js
@@ -0,0 +1,19 @@
+exports.about = function(req, res){
+  res.render('about');      
+};
+
+exports.insurance = function(req, res){
+  res.render('insurance');      
+};
+
+exports.contact = function(req, res){
+  res.render('contact');      
+};
+
+exports.news = function(req, res){
+  res.render('news');      
+};
+
+exports.products = function(req, res){
+  res.render('products');      
+};
